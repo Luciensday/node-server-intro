1ff11308282c789650070ff2819aa5b71b5cdeac
Lucien Lin <luciensday@gmail.com>
2024-01-17 17:55:10 +0000

    middleware

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const server = express();
 
+function logger(request, response, next) {
+  console.log(request.method + " " + request.url);
+  next();
+}
+
+server.use(logger);
+
 server.get("/", (request, response) => {
-  const year = new Date().getFullYear();
-  response.send(`
-  <!doctype html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title>Home</title>
-    </head>
-    <body>
-      <h1>Hello, it's ${year}</h1>
-    </body>
-  </html>
-  `);
+  response.send("<h1>Hello</h1>");
 });
+
+// server.get("/", (request, response) => {
+//   const year = new Date().getFullYear();
+//   response.send(`
+//   <!doctype html>
+//   <html>
+//     <head>
+//       <meta charset="utf-8">
+//       <title>Home</title>
+//     </head>
+//     <body>
+//       <h1>Hello, it's ${year}</h1>
+//     </body>
+//   </html>
+//   `);
+// });
 
 server.get("/un-oh", (request, response) => {
   response.status(500);
